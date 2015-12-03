5598f02c37a315a0a8b57dcc49fa25940bd5fe5a
Harris Tzovanakis <me@drjova.com>
2015-12-03 17:28:43 +0100

    elasticsearch: fallback file per hostname
    * Adds `CFG_ELASTICSEARCH_FALLBACK_DIRECTORY` configuration variable
      to specify where the fallback file to be stored.
    
    * Creates a fallback file with the hostname as file name.
    
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- modules/miscutil/lib/elasticsearch_logging.py
+++ modules/miscutil/lib/elasticsearch_logging.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2014 CERN.
+## Copyright (C) 2014, 2015 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -17,20 +17,21 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-__revision__ = \
-   "$Id$"
-
-from invenio.config import \
-     CFG_ELASTICSEARCH_LOGGING, \
-     CFG_ELASTICSEARCH_INDEX_PREFIX, \
-     CFG_ELASTICSEARCH_HOSTS, \
-     CFG_ELASTICSEARCH_SUFFIX_FORMAT, \
-     CFG_ELASTICSEARCH_MAX_QUEUE_LENGTH, \
-     CFG_ELASTICSEARCH_FLUSH_INTERVAL
+from invenio.config import (
+    CFG_ELASTICSEARCH_FALLBACK_DIRECTORY,
+    CFG_ELASTICSEARCH_FLUSH_INTERVAL,
+    CFG_ELASTICSEARCH_HOSTS,
+    CFG_ELASTICSEARCH_INDEX_PREFIX,
+    CFG_ELASTICSEARCH_LOGGING,
+    CFG_ELASTICSEARCH_MAX_QUEUE_LENGTH,
+    CFG_ELASTICSEARCH_SUFFIX_FORMAT,
+)
 
 if CFG_ELASTICSEARCH_LOGGING:
-    import lumberjack
     import logging
+    import lumberjack
+    import os
+    import socket
     import sys
 
 def initialise_lumberjack():
@@ -49,6 +50,18 @@ def initialise_lumberjack():
     else:
         config['interval'] = CFG_ELASTICSEARCH_FLUSH_INTERVAL
 
+    # Check if lumberjack directory exists
+    _lumberjack_exists(CFG_ELASTICSEARCH_FALLBACK_DIRECTORY)
+
+    # Get hostname from socket (strip cern.ch)
+    fallback_file_name = socket.getfqdn().replace('.cern.ch', '')
+
+    fallback_file_path = os.path.join(
+        CFG_ELASTICSEARCH_FALLBACK_DIRECTORY,
+        fallback_file_name
+    )
+    config['fallback_log_file'] = fallback_file_path
+
     lj = lumberjack.Lumberjack(
         hosts=CFG_ELASTICSEARCH_HOSTS,
         config=config)
@@ -63,7 +76,20 @@ def initialise_lumberjack():
 
     return lj
 
+
+def _lumberjack_exists(path):
+    """Check if lumberjack directory exists.
+
+    :param str path: the path to lumberjack directory
+    """
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
+
 LUMBERJACK = initialise_lumberjack()
+
 
 def register_schema(*args, **kwargs):
     if not CFG_ELASTICSEARCH_LOGGING:
